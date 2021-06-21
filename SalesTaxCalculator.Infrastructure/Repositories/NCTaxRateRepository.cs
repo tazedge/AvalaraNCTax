6f523091d64114f1b81228a36a826a76d9a6ef49
tazedge <86213967+tazedge@users.noreply.github.com>
2021-06-21 00:57:40 -0400

    Add files via upload

---
+++ SalesTaxCalculator.Infrastructure/Repositories/NCTaxRateRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SalesTaxCalculator.Domain.Interfaces;
+using SalesTaxCalculator.Domain.Model;
+
+namespace SalesTaxCalculator.Infrastructure.Repositories
+{
+    public class NCTaxRateRepository : INCTaxRateRepository
+    {
+
+        private readonly ICsvFileReader _csvReader;
+        private List<NCCountyTax> _NCTaxList;
+
+        public NCTaxRateRepository(ICsvFileReader csvReader)
+        {
+            _csvReader = csvReader;
+            _NCTaxList = new List<NCCountyTax>();
+        }
+        public decimal GetRateForCounty(string countyName)
+        {
+            if (_NCTaxList.Count == 0)
+            {
+                LoadNCTaxRates();
+            }
+
+            decimal rate = 0;
+            foreach (var ncCountyTax in _NCTaxList)
+            {
+                if (ncCountyTax.CountyName.ToLower().Contains(countyName.ToLower()))
+                {
+                    rate = ncCountyTax.TaxRate;
+                    break;
+                }
+                  
+            }
+
+            return rate;
+        }
+
+        private void LoadNCTaxRates()
+        {
+ 
+            var dataPath = "NCTaxRates.csv";
+            _NCTaxList.Clear();
+            _NCTaxList = _csvReader.ReadRecords<NCCountyTax>(
+                dataPath,  
+                false,
+                ",", 
+                0, 
+                1)
+                .ToList();
+
+        }
+    }
+}
